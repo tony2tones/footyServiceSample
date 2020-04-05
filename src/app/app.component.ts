8d017daf3ec93b6618ca6207a2bde276a8449310
Anthony <anthonymarques@outlook.com>
2020-04-05 19:38:51 +0200

    fixed up calls cleaned up code added comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from "@angular/core";
 import { FootyService } from "./footy.service";
-import { ResponseLeague, League } from './interface';
 
 @Component({
   selector: "app-root",
@@ -13,16 +12,19 @@ export class AppComponent {
   position:string
   gamesPlayed:string
   wins:string
+  // constructor imports footy service...the footy can 
+  // be named into anything by the way.
   constructor(private footy: FootyService) {}
 
   clicker() {
+    // Use that footy service and call the functon to 
+    // return a value that you can subscribe to
     this.footy.getLeagueStand().subscribe(data => {
       this.teamName = data[0].team_name;
       this.position = data[0].overall_league_position;
       this.gamesPlayed = data[0].overall_league_payed;
       this.wins = data[0].overall_league_W;
 
-      console.log('my response', this.teamName);
       console.log('from data response', data);
     });
   }
--- src/app/footy.service.ts
+++ src/app/footy.service.ts
@@ -5,7 +5,10 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root"
 })
 export class FootyService {
-  apiKey = "1a1a8efa50b6a6f8f1eca1628b95240ec7c2c905934740a5780f4b051195c555";
+  // place your apikey below
+  apiKey = "APIKEY_IN_HERE_GUYS";
+  // you can keep all your constants in variables to make the url calls
+  // easier to manage
   url =
     "https://apifootball.com/api/?action=get_standings&league_id=62&APIkey=";
 
