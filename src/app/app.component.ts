27f7572e8cf03f0b46c238cc318d19b6f96811c0
Anthony <anthonymarques@outlook.com>
2020-07-02 23:01:30 +0200

    Added mock data, incase endpoints expired or other issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,44 @@
 import { Component } from "@angular/core";
 import { FootyService } from "./footy.service";
 
+// MOCK DATA
+const teamData = 
+  {
+    teamName: "Liverpool",
+    position: "1",
+    gamesPlayed: "loads",
+    wins: "28",
+    draws: "2",
+    loses: "3",
+  };
+
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   title = "servicesFooty";
-  teamName:string
-  position:string
-  gamesPlayed:string
-  wins:string
-  // constructor imports footy service...the footy can 
+  teamName: string;
+  position: string;
+  gamesPlayed: string;
+  wins: string;
+
+  // constructor imports footy service...the footy can
   // be named into anything by the way.
   constructor(private footy: FootyService) {}
 
   clicker() {
-    // Use that footy service and call the functon to 
+    // Use that footy service and call the functon to
     // return a value that you can subscribe to
-    this.footy.getLeagueStand().subscribe(data => {
-      this.teamName = data[0].team_name;
-      this.position = data[0].overall_league_position;
-      this.gamesPlayed = data[0].overall_league_payed;
-      this.wins = data[0].overall_league_W;
+    // this.footy.getLeagueStand().subscribe((data) => {
+      this.teamName = teamData.teamName;
+      this.position = teamData.position;
+      this.gamesPlayed = teamData.gamesPlayed;
+      this.wins = teamData.wins;
 
-      console.log('from data response', data);
-    });
+      console.log("from data response", teamData);
+    // });
   }
 }
